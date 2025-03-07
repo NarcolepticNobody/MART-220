d6c274b0d5d93e3ab2b8c05a47c94a7eb49981ba
NarcolepticNobody <alexis.huotte@umontana.edu>
2025-03-07 12:10:16 -0700

    ok

---
+++ week6troubleshoot/week 6 actually 5/troubleshoot_character.js
@@ -0,0 +1,53 @@
+class character
+{
+    constructor(path, x, y)
+    {
+        this.path = path;
+        this.x = x;
+        this.y = y;
+        this.imageWidth = 100;
+        this.imageHeight = 100;
+        this.flipX = false;
+        this.flipRun = false;
+
+
+
+        // need the image
+        this.myImage = loadImage(this.path);
+    }
+
+    draw()
+    {
+       
+    
+        push();
+        if (this.flipX) {
+        translate(this.imageWidth, 0);
+        scale(-1, 1);
+        image(this.myImage, -this.x, this.y, 100, 100); 
+
+        }
+        else {
+        image(this.myImage, this.x, this.y, 100, 100);
+        } 
+        pop();
+
+        
+
+     
+    }
+
+
+
+    hasCollided(x2, y2, w2, h2) {
+    return (
+
+
+        this.x < x2 + w2 &&
+        this.x + this.imageWidth > x2 &&
+        this.y < y2 + h2 &&
+        this.y + this.imageHeight > y2 
+    );
+
+}
+}
