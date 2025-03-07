f1da4477159737bdd492f3e6a60d88a756162c2f
NarcolepticNobody <alexis.huotte@umontana.edu>
2025-03-07 12:31:33 -0700

    ok

---
+++ Homework 7/food.js
@@ -0,0 +1,17 @@
+class food
+{
+    constructor(x,y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    draw()
+    {
+        fill(22, 1, 120);
+        circle(this.x, this.y, 50);
+    }
+
+
+
+}
