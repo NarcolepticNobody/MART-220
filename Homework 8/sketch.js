c49ec23f28a9c743497a0c49a7362273def449fb
NarcolepticNobody <alexis.huotte@umontana.edu>
2025-03-30 11:37:37 -0600

    hm

--- Homework 8/sketch.js
+++ Homework 8/sketch.js
@@ -1,336 +1,229 @@
-//character
+// Global Variables
 var animation = [];
-var loadAnimation = [];
 var run = [];
-var runAnimation = [];
 var idleStrings = [];
 var runStrings = [];
-var moveCharacter;
-var currentFrame;
-var checkCollision;
-//character stuff collision with food
 var foodArray = [];
-//cordinates?
-var i = 0;
-var k = 0;
-var x = 200;
-var y = 90;
-var r = 0; //idk why I need this
-
+var trees = [];
+var logs = [];
 var flipX = false;
 var moving = false;
-//movement
-var kb = [];
-//time stuff
-var countDown = [];
-let score = 0;
-let startTime;
-let elapsedTime = 0;
-let countdown = 20;
-let timeLeft = 20; 
-//sounds
-var mySound;
-var song;
-let good;
-let bad;
-let end;
-//health 
-var foodHealth;
-let health = 50;
-let maxHealth = 100;
+var score = 0;
+var countdown = 20;
+var timeLeft = 20;
+var health = 50;
+var maxHealth = 100;
+var startTime;
+var img, img2;
+let good, bad, end, song;
+var myAnimation;
 
-
-
-var loadImage;
-var treeStrings = [];
-var logStrings = [];
-
-let trees = [];
-let logs = [];
-
-
-
+// Preload assets
 function preload() {
-    
     soundFormats('mp3', 'ogg', 'wav');
-    //background music
     song = loadSound("assets/melody.mp3");
-    //good food
-    good = loadSound("assets/yes.mp3")
-    //bad food
-    bad = loadSound("assets/442602__topschool__ow-sound.mp3")
-    //failed
-    end = loadSound("assets/wampp.mp3")
-    
+    good = loadSound("assets/yes.mp3");
+    bad = loadSound("assets/442602__topschool__ow-sound.mp3");
+    end = loadSound("assets/wampp.mp3");
     idleStrings = loadStrings('idle.txt');
-
-    runStrings = loadStrings('run.txt'); 
-
-    treeStrings = loadStrings('tree.txt');
-
-    logStrings = loadStrings('log.txt');
-
-    attackStrings = loadStrings('attack.txt');
-
+    runStrings = loadStrings('run.txt');
     img = loadImage('trees/PineTree 01.png');
-
-    img2 = loadImage('assets/log.png')
-
-   /* let tree = createSprite(x, y, 50, 50);
-    tree.addImage(PineTree.png);
-    tree.scale = 1.0;
-    tree.collider = 'static';
-    tree.push(tree);
-   */
+    img2 = loadImage('assets/log.png');
 }
 
-
+// Setup function
 function setup() {
     createCanvas(1280, 450);
-    startTime = millis(); 
-    myAnimation = new character(200, 200);
+    startTime = millis();
+    myAnimation = new Character(100, 100);
     myAnimation.loadAnimation('idle', idleStrings);
-    myAnimation.loadAnimation('run', runStrings); 
-    //myAnimation.loadAnimation('tree', treeStrings); //strings don't work
-    //myAnimation.loadAnimation('log', logStrings); //strings don't work
-    
-    
-    
-   // Create food objects
-   for (let i = 0; i < 20; i++) {
-    if(floor(random(0,2)) == 0) {
-        myFood = new food(random(50, width - 50), random(50, height - 50), true);
-        
-    }
-    else {
-        myFood = new food(random(50, width - 50), random(50, height - 50), true);
+    myAnimation.loadAnimation('run', runStrings);
+
+
+
+
+
+    // Create food objects
+    for (let i = 0; i < 20; i++) {
+        let isGood = random([true, false]);
+        let myFood = new Food(random(50, width - 50), random(50, height - 50), isGood);
         foodArray.push(myFood);
     }
-    let timePassed = int((millis() - startTime) / 1500);
-    timeLeft = max(countdown - timePassed, 0); // Prevents negative values
-   }
 
 
 
-     // Initialize trees     //I want these to be random
-     trees = [
-        new tree(300, 40, 190, 140),
-        new tree(400, 200, 190, 140),
-        new tree(650, 100, 130, 110),
-        new tree(900, 200, 160, 120),
-        new tree(1000, 1, 100, 100),
-        new tree(1070, 40, 100, 100)
-    ];
-   
-
-    // Initialize logs          //I want these to be random
-    logs = [
-        new log(70, 100, 100, 100),
-        new log(200, 300, 100, 100),
-        new log(400, 300, 100, 100),
-        new log(600, 100, 100, 100),
-        new log(800, 300, 150, 100)
-    ];
+   // Create static trees
+   trees = [
+    new Tree(300, 40, 50, 100),
+    new Tree(400, 200, 60, 110),
+    new Tree(650, 100, 50, 100),
+    new Tree(900, 200, 70, 120),
+    new Tree(1000, 1, 80, 130),
+    new Tree(1070, 40, 90, 140)
+];
 }
 
 
 
-function draw() {   
-
+// Draw function
+function draw() {
     background(40, 100, 10);
-
     updateHealth(health, maxHealth);
+    collidesWithTree();
 
+    // Border
+    push();
     stroke(0);
-
-    strokeWeight(1);
-
-   //moveCharacter();
-
-    //myAnimation();
-
-    //checkCollision();
-
-    //setCollider();
-
-    
-    //Border 
-        push(); 
-        stroke(0); 
-        strokeWeight(10); 
-        noFill();
-        rect(0, 0, width, height);
-        pop(); 
-
+    strokeWeight(10);
+    noFill();
+    rect(0, 0, width, height);
+    pop();
 
     // Display trees
     for (let tree of trees) {
         tree.display(img);
     }
 
-    // Display logs
-    for (let log of logs) {
-        log.display(img2);
-    }
- 
-  //Health bar 
-      textSize(20);
-      fill(255, 50, 100)
-      text("Health bar", 600, 440); 
-  //Feed the Dino
-      textSize(30);
-      fill(0, 0, 0)
-      text("Feed the Dino!", width / 2 - 100, height / 12); 
-  
-  // Display countdown timer
-      textSize(20);
-      fill(255);
-      text("Score: " + score, 400, 30);
-      text("Time Left: " + timeLeft + "s", width - 500, 30);  
-  }
+    // Display food
+    for (let i = 0; i < foodArray.length; i++) {
+        foodArray[i].draw();
 
-    // Check for win condition
-    if (score >= 20) {
-    textSize(60);
-    fill(255, 215, 0);
-    text("YOU WIN!", width / 2 - 125, height / 2);
-    noLoop(); // Stop the game
-    
-}
-    // Check if time is up 
-    if (timeLeft <= 1) {
-    textSize(50);
-    fill(255, 0, 0)
-    text("You Big Lose!", width / 2 - 125, height / 2);
-    noLoop(); // Stop the game when time runs out
-
-     
- }
-   
-function updateHealth(health, maxHealth) {
-   stroke(0);
-   strokeWeight(4);
-   noFill();
-   rect(500, 400, 300, 10, map(health, 0, maxHealth, 0, 200),15);
-   noStroke();
-   fill(233, 0, 0);
-   
-}
-
-function foodArray(i) {
-   if (good.play());
-    health += 10;
-    good.setLoop(false);
-    good.remove();
-    good.position.x = 10;
-}
-   
-//food array and collision
-for (let j = 0; j < foodArray.length; j++) {
-    if (myAnimation.isColliding(foodArray[j].foodPiece)) {
-        
-        if (foodArray[j].isGood) {
-            score++;
-        } else {
-            score--;
-        }
-
-       
-
-        foodArray[j].foodPiece.remove();
-    }
-    
-}
-
-if (kb.pressing('d')) {
-    myAnimation.updatePosition('forward');
-    myAnimation.draw('run');
-   
-}
-else if (kb.pressing('a')) {
-    myAnimation.updatePosition('reverse');
-    myAnimation.draw('run');
-}
-else if (kb.pressing('w')) {
-    myAnimation.updatePosition('up');
-    myAnimation.draw('run');
-}
-else if (kb.pressing('s')) {
-    myAnimation.updatePosition('down');
-    myAnimation.draw('run');
-}
-else {
-
-    myAnimation.draw('idle');
-}
-
-for(let i = 0; i < foodArray.length; i++) {
-
- foodArray[i].draw(); 
-
-  if(myAnimation.isColliding(foodArray[i].foodPiece))
-     {
-        if(foodArray[i].isGood)
-            {
+        // Check collision with character
+        if (myAnimation.isColliding(foodArray[i])) {
+            if (foodArray[i].isGood) {
                 score++;
-            }
-            else
-            {
+                health = min(health + 10, maxHealth);
+                good.play();
+            } else {
                 score--;
+                health = max(health - 10, 0);
+                bad.play();
             }
-            foodArray[i].foodPiece.remove();
-     } 
+            foodArray.splice(i, 1);
+        }
     }
 
-function displayFood() {
-    for (let i = 0; i < foodArray.length; i++) {
-    foodArray[i].draw();
-}
-
-}
-
-function updateIndex() {
-    i++;
-     if (i > idleStrings.length-1) {
-        i = 0;
+    // Keyboard controls
+    if (keyIsDown(68)) { // 'D' key
+        myAnimation.updatePosition('forward');
+        myAnimation.draw('run');
+    } else if (keyIsDown(65)) { // 'A' key
+        myAnimation.updatePosition('reverse');
+        myAnimation.draw('run');
+    } else if (keyIsDown(87)) { // 'W' key
+        myAnimation.updatePosition('up');
+        myAnimation.draw('run');
+    } else if (keyIsDown(83)) { // 'S' key
+        myAnimation.updatePosition('down');
+        myAnimation.draw('run');
+    } else {
+        myAnimation.draw('idle');
+    }
+    function collidesWithTree(newX, newY) {
+        for (let tree of trees) {
+            if (newX + 40 > tree.x && newX < tree.x + tree.w &&
+                newY + 50 > tree.y && newY < tree.y + tree.h) {
+                return true; // Collision detected
+            }
+        }
+        return false;
      }
+     
 
-}
+    // Display UI
+    textSize(20);
+    fill(255);
+    text("Score: " + score, 400, 30);
+    text("Time Left: " + timeLeft + "s", width - 500, 30);
 
-function updateCountDown() {
-    countDown--;
-if (countDown == 0) {
-    clearInterval(countDownInterval);
-}
-}
+    // Check win condition
+    if (score >= 20) {
+        textSize(60);
+        fill(255, 215, 0);
+        text("YOU WIN!", width / 2 - 125, height / 2);
+        noLoop();
+    }
 
-// Idle animation update
-function updateIdleIndex() {
-    if (moving) {
-        i = (i + 1) % animation.length; //this was animation.length
+    // Check time left
+    let timePassed = int((millis() - startTime) / 1000);
+    timeLeft = max(countdown - timePassed, 0);
+
+    if (timeLeft <= 0) {
+        textSize(50);
+        fill(255, 0, 0);
+        text("You Big Lose!", width / 2 - 125, height / 2);
+        noLoop();
     }
 }
 
-// Running animation update
-function updateRunIndex() {
-    if (moving) {
-        k = (k + 1) % run.length;
+// Update health bar
+function updateHealth(health, maxHealth) {
+    stroke(0);
+    strokeWeight(4);
+    noFill();
+    rect(500, 400, 300, 10);
+    noStroke();
+    fill(233, 0, 0);
+    rect(500, 400, map(health, 0, maxHealth, 0, 300), 10);
+}
+
+// Character Class
+class Character {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.animations = {};
+    }
+
+    loadAnimation(character, frames) {
+        this.animations[char] = frames;
+    }
+
+    draw(state) {
+        textSize(20);
+        fill(255);
+        text(state, this.x, this.y - 20);
+    }
+
+    updatePosition(direction) {
+        let speed = 5;
+        if (direction === 'forward') this.x += speed;
+        if (direction === 'reverse') this.x -= speed;
+        if (direction === 'up') this.y -= speed;
+        if (direction === 'down') this.y += speed;
+        this.x = constrain(this.x, 0, width);
+        this.y = constrain(this.y, 0, height);
+    }
+
+    isColliding(food) {
+        return dist(this.x, this.y, food.x, food.y) < 20;
     }
 }
 
+// Food Class
+class Food {
+    constructor(x, y, isGood) {
+        this.x = x;
+        this.y = y;
+        this.isGood = isGood;
+    }
 
-
-function loadFood() {
-    for (let i = 0; i < foodArray.length -1; i++) {
-
-    foodArray[i].draw();
+    draw() {
+        fill(this.isGood ? 'green' : 'red');
+        ellipse(this.x, this.y, 20, 20);
+    }
 }
-function foodFight() {
-    for (let i = 0; i < foodArray.length; i++) {
-        foodArray[i].x = random(100,200);
-        foodArray[i].y = random(300,400);
-    } 
-    
-   
-}
+
+// Tree Class
+class Tree {
+    constructor(x, y, w, h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+
+    display(img) {
+        image(img, this.x, this.y, this.w, this.h);
+    }
 }
